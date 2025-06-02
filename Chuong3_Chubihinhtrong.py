1b8d2d524081c3862743fd998dbd4be0fab54286
tuyen <trongtuyendo7@gmail.com>
2025-06-02 18:57:58 +0900

    Updat bai tap

---
+++ Chuong3_Chubihinhtrong.py
@@ -0,0 +1,8 @@
+# Nhập dữ liệu cho biến name từ bàn phím
+
+def modulo_result(x):
+    p = 2 * x * 3.14
+    print("p = " + str(round(p, 2)))
+    
+a = float(input("Nhập bán kính a: "))
+modulo_result(a)
