ed07af4f9a66a252e9d7f0949f9dd8c7112d1cb3
tuyen <trongtuyendo7@gmail.com>
2025-06-03 18:34:27 +0900

    update 03062505

---
+++ Chuong5_B1_ifelse.py
@@ -0,0 +1,6 @@
+age = int(input("Age cat is "))
+
+if age <= 5:
+    print("Your cat is young")
+else:
+    print("Your cat is old")
---
+++ Chuong5_B3_Sosanh3so.py
@@ -0,0 +1,14 @@
+x = int(input("input x = "))
+y = int(input("input y = "))
+z = int(input("input z = "))
+
+if x % 2 == 0:
+    if y >= 20:
+        print("y is greater than or equal to 20")
+    else: 
+        print("y is less than 20")
+else: 
+    if z >= 30:
+        print("z is greater than or equal to 30")
+    else: 
+        print("z is less than 30")
